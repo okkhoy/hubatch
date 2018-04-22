9b5a223110c5d5da80cc745b9286294e6b6dbd72
Akshay Narayan <okkhoy@gmail.com>
2018-04-22 12:16:44 +0800

    comment out mention in the new issue created.
    we don't want to mention the tester now.

--- controllers/issues.py
+++ controllers/issues.py
@@ -108,7 +108,7 @@ class IssueController(BaseController):
             else:
                 self.statistics[to_mapping[1]]["count"] += 1
                 self.statistics[to_mapping[1]]["team"].append(to_mapping[0])
-            new_body = new_body + '\n\n Issue created by: @' + to_mapping[1]
+            # new_body = new_body + '\n\n Issue created by: @' + to_mapping[1]
 
             labels = [org_team[to_mapping[0]], org_tutorial[to_mapping[0]]]
             is_transferred = self.ghc.create_issue(new_title, new_body, None, labels, actl_to_repo)
